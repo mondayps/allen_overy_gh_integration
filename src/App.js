a757bf7dcdf73103692f115ab4ebd51dd2ff5a8d
Cedric Falcou <cedricfa@monday.com>
2023-02-17 19:00:27 +0000

    Refactor without library

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import "./App.css"
 import mondaySdk from "monday-sdk-js"
 import "monday-ui-react-core/dist/main.css"
-import Delta from "quill-delta"
 
 //Explore more Monday React Components here: https://style.monday.com/
 import { Button } from "monday-ui-react-core"
@@ -17,50 +16,25 @@ const App = () => {
 
       // set context
       const currentContext = await monday.get("context")
-      alert("test")
-      const { range, focusedBlocks } = currentContext.data
+      const currentBlock =
+        currentContext.data.focusedBlocks[0].content.deltaFormat
 
-      // loop through "focusedBlocks" to update each block in the highlighted text
-      focusedBlocks.forEach(async (block) => {
-        let original = new Delta(block.content.deltaFormat)
-
-        // make elements of the current block all uppercase
-        const updated = original.map((op) => {
-          if (range !== null) {
-            const start = range.index
-            const end = range.index + range.length
-            const newInsert =
-              op.insert.substring(0, start) +
-              op.insert.substring(start, end).toUpperCase() +
-              op.insert.substring(end, op.insert.length)
-            const ret = {
-              insert: newInsert,
-              attributes: op?.attributes,
-            }
-            return ret
-          } else if (typeof op.insert === "string") {
-            const ret = {
-              insert: op.insert.toUpperCase(),
-              attributes: op?.attributes,
-            }
-            return ret
-          } else {
-            return ""
-          }
-        })
-
-        // Use monday.execute() to update the current block
-        const id = block.id
-        const content = {
-          deltaFormat: updated,
+      const newBlock = []
+      currentBlock.forEach(async (currentWord) => {
+        const newWord = {
+          insert: currentWord.insert.toUpperCase(),
+          attributes: currentWord.attributes,
         }
-        const data = await monday.execute("updateDocBlock", { id, content })
-
-        return data
+        newBlock.push(newWord)
       })
 
-      // Use monday.execute() to close the app modal once the action is complete
-      monday.execute("closeDocModal")
+      const id = currentContext.data.focusedBlocks[0].id
+      const content = {
+        deltaFormat: newBlock,
+      }
+      const data = await monday.execute("updateDocBlock", { id, content })
+      await monday.execute("closeDocModal")
+      return data
     } catch (error) {
       console.log(`There was an error: ${error}`)
     }
